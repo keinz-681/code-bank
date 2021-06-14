067070a5f699baa4d1a7b25a185639396b728097
KEINZ <76252197+keinz-681@users.noreply.github.com>
2021-06-14 20:38:55 +0900

    1※あげ

---
+++ Unitytango.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int main(void)
+{
+    int a;//申し訳程度の
+    /*最上位ウィンドウの和訳*/
+    printf("1.File:ファイル\n");
+    printf("2.Edit:編集\n");
+    printf("3.Assets:アセット\n");
+    printf("4.GameObject:ゲームオブジェクト\n");
+    printf("5.Component:構成要素\n");
+    printf("6.Tutorial:チュートリアル\n");
+    printf("7.VR Beginner:VRビギナー\n");
+    printf("8.Winsow:ウィンドウ\n");
+    printf("9.Help:お助け\n");
+    printf("更に詳細の場合は 0 を押してください:",a); scanf("%d",&a);
+    /*怒涛のif文エリア*/
+    if (a=1)//Fileを選んだら
+    {
+        /* codeは11項目 */
+        printf("1.\n");
+        printf("2.\n");
+        printf("3.\n");
+        printf("4.\n");
+        printf("5.\n");
+        printf("6.\n");
+        printf("7.\n");
+        printf("8.\n");
+        printf("9.\n");
+        printf("10.\n");
+        printf("11.\n");
+    }
+    if (a=2)
+    {
+        /* code */
+    }
+    if (a=3)
+    {
+        /* code */
+    }
+    if (a=4)
+    {
+        /* code */
+    }
+    if (a=5)
+    {
+        /* code */
+    }
+    if (a=6)
+    {
+        /* code */
+    }
+    if (a=7)
+    {
+        /* code */
+    }
+    if (a=8)
+    {
+        /* code */
+    }
+    if (a=9)
+    {
+        /* code */
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
