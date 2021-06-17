ed11b9adc22c8004f9c5e694d979674fb763487f
KEINZ <76252197+keinz-681@users.noreply.github.com>
2021-06-17 18:41:37 +0900

    Add files via upload

---
+++ range.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+弧のカーブを算出する公式
+R=((w/2)^2+h^2)/2h
+※わっかり辛くてﾒﾝｺﾞ
+*/
+int main(void)
+{
+    double w,h;
+    printf("w= ");
+    scanf("%lf",&w);
+    printf("h= ");
+    scanf("%lf",&h);
+    double a=w/2;
+    double aa=a*a;
+    double b=h*h;
+    double d=2*h;
+    double e=aa+b;
+    double r=e/d;
+    printf("R=%f",r);
+    return 0;
+}
